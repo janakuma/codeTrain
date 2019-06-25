033ecc1342645e9690753af39d30820df6c2fe61
BLUEHOLE\janakuma <janakuma@bluehole.net>
2019-06-25 11:23:24 +0900

    작업

--- jsStudy/inside_javascript/Chapter_05/ex5-10.js
+++ jsStudy/inside_javascript/Chapter_05/ex5-10.js
@@ -5,7 +5,7 @@ function HelloFunc(func) {
 
 HelloFunc.prototype.call = function(func) {
 	func ? func(this.greeting) : this.func(this.greeting);
-}	
+}
 
 var userFunc = function(greeting) {
 	console.log(greeting);
